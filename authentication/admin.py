887c30836182378ee2d5e58d2ec97abb80d72b7f
sattorov <sattorov.ahu@gmail.com>
2024-12-13 20:47:25 +0500

    created models

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,6 +4,6 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'username', 'phone_number')
+    list_display = ('id', 'first_name', 'last_name', 'phone_number')
     list_display_links = ('id', 'first_name')
-    search_fields = ('first_name', 'last_name', 'username', 'phone_number')
+    search_fields = ('first_name', 'last_name', 'phone_number')

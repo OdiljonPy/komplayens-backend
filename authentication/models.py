fa36a63819fbba14f832d2fe4fe1534e2c62022d
sattorov <sattorov.ahu@gmail.com>
2024-12-13 18:55:20 +0500

    created models

--- authentication/models.py
+++ authentication/models.py
@@ -8,6 +8,12 @@ ROLE_CHOICES = (
     (4, 'Employee'),
 )
 
+LOGIN_REQUEST_STATUS = (
+    (1, 'Waiting'),
+    (2, 'Approved'),
+    (3, 'Rejected'),
+)
+
 
 class User(BaseModel):
     first_name = models.CharField(max_length=80, verbose_name='Имя')
@@ -19,6 +25,7 @@ class User(BaseModel):
         to='services.Organization', null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Организация")
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=2, verbose_name="Роль")
     login_time = models.DateTimeField(null=True, verbose_name="Время входа")
+    active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['full_name']
@@ -29,4 +36,17 @@ class User(BaseModel):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ('-created_at',)
+
+
+class LoginRequest(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    status = models.IntegerField(choices=LOGIN_REQUEST_STATUS, default=1, verbose_name='Статус')
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = 'Запрос на вход'
+        verbose_name_plural = 'Запросы на вход'
         ordering = ('-created_at',)

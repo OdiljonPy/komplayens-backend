25da525d09681aefa59174d55e2ad97e22ff3dd7
otabek <baqara163@gmail.com>
2024-12-14 15:07:13 +0500

    serializer and  views added for base app

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,29 @@
 from django.urls import path
+from .views import (RegionViewSet, DistrictViewSet, FAQViewSet,
+                    AboutUsViewSet, CorruptionCaseViewSet, CorruptionRiskViewSet)
 
 urlpatterns = [
-
+    path('regions/', RegionViewSet.as_view({'get': 'list', 'post': 'create'}), name='regions_list'),
+    path('region/<int:pk>/',
+         RegionViewSet.as_view({'get': 'detail_', 'patch': 'update', 'delete': 'delete'}),
+         name='region_funcs'),
+    path('districts/', DistrictViewSet.as_view({'get': 'list', 'post': 'create'}), name='districts_list'),
+    path('district/<int:pk>/',
+         DistrictViewSet.as_view({'get': 'detail_', 'patch': 'update', 'delete': 'delete'}),
+         name='district_funcs'),
+    path('faqs/', FAQViewSet.as_view({'get': 'list', 'post': 'create'}), name='faq_list'),
+    path('faq/<int:pk>/',
+         FAQViewSet.as_view({'get': 'detail_', 'patch': 'update', 'delete': 'delete'}),
+         name='faq_funcs'),
+    path('about/', AboutUsViewSet.as_view({'get': 'last', 'post': 'create'}), name='last_about'),
+    path('about/<int:pk>/', AboutUsViewSet.as_view({'patch': 'update', 'delete': 'delete'}),
+         name='about_funcs'),
+    path('corruptioncase/', CorruptionCaseViewSet.as_view({'get': 'list', 'post': 'create'}), name='corruption_case_list'),
+    path('corruptioncase/<int:pk>/',
+         CorruptionCaseViewSet.as_view({'get': 'detail_', 'patch': 'update', 'delete': 'delete'}),
+         name='corruption_case_funcs'),
+    path('corruptionrisk/', CorruptionRiskViewSet.as_view({'get': 'list', 'post': 'create'}), name='corruption_risk_list'),
+    path('corruptionrisk/<int:pk>/',
+         CorruptionRiskViewSet.as_view({'get': 'detail_', 'patch': 'update', 'delete': 'delete'}),
+         name='corruption_risk_funcs'),
 ]
--- base/views.py
+++ base/views.py
@@ -12,7 +12,6 @@ from exceptions.error_messages import ErrorCodes
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework import status
-from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 
@@ -34,7 +33,7 @@ class RegionViewSet(ViewSet):
         responses={200: RegionSerializer()},
         tags=['Region']
     )
-    def detail(self, request, pk=None):
+    def detail_(self, request, pk=None):
         region = Region.objects.filter(id=pk).first()
         if region is None:
             raise CustomApiException(ErrorCodes.NOT_FOUND, message='Region not found')
@@ -104,7 +103,7 @@ class DistrictViewSet(ViewSet):
         responses={200: DistrictSerializer()},
         tags=['District']
     )
-    def detail(self, request, pk=None):
+    def detail_(self, request, pk=None):
         district = District.objects.filter(id=pk).first()
         if district is None:
             raise CustomApiException(ErrorCodes.NOT_FOUND, message='District not found')
@@ -174,7 +173,7 @@ class FAQViewSet(ViewSet):
         responses={200: FAQSerializer()},
         tags=['FAQ']
     )
-    def detail(self, request, pk=None):
+    def detail_(self, request, pk=None):
         faq = FAQ.objects.filter(id=pk).first()
         if faq is None:
             raise CustomApiException(ErrorCodes.NOT_FOUND, message='FAQ not found')
@@ -223,4 +222,204 @@ class FAQViewSet(ViewSet):
         if faq is None:
             raise CustomApiException(ErrorCodes.NOT_FOUND, message='FAQ not found')
         faq.delete()
+        return Response(data={'result': '', 'ok': True}, status=status.HTTP_200_OK)
+
+
+class AboutUsViewSet(ViewSet):
+    @swagger_auto_schema(
+        operation_summary='Get last About Us',
+        operation_description='Get last About Us',
+        responses={200: AboutUsSerializer()},
+        tags=['AboutUs']
+    )
+    def last(self, request):
+        about_us = AboutUs.objects.order_by('-created_at').first()
+        serializer = AboutUsSerializer(about_us, context={'request': request})
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Create About Us',
+        operation_description='Create a new About Us',
+        request_body=AboutUsSerializer(),
+        responses={200: AboutUsSerializer()},
+        tags=['AboutUs']
+    )
+    def create(self, request):
+        data = request.data
+        serializer = AboutUsSerializer(data=data, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Update About Us',
+        operation_description='Update a About Us',
+        request_body=AboutUsSerializer(),
+        responses={200: AboutUsSerializer()},
+        tags=['AboutUs']
+    )
+
+    def update(self, request, pk=None):
+        about_us = AboutUs.objects.filter(id=pk).first()
+        if about_us is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='About not found')
+        serializer = AboutUsSerializer(about_us, data=request.data, partial=True, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Delete About Us',
+        operation_description='Delete a About Us',
+        tags=['AboutUs']
+    )
+    def delete(self, request, pk=None):
+        about_us = AboutUs.objects.filter(id=pk).first()
+        if about_us is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='About not found')
+        about_us.delete()
+        return Response(data={'result': '', 'ok': True}, status=status.HTTP_200_OK)
+
+
+class CorruptionRiskViewSet(ViewSet):
+    @swagger_auto_schema(
+        operation_summary='Corruption Risk',
+        operation_description='Corruption Risk List',
+        responses={200: CorruptionRiskSerializer(many=True)},
+        tags=['CorruptionRisk']
+    )
+    def list(self, request):
+        corruption_risk = CorruptionRisk.objects.all()
+        serializer = CorruptionRiskSerializer(corruption_risk, many=True, context={'request': request})
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Corruption Risk',
+        operation_description='Corruption Risk Details',
+        responses={200: CorruptionRiskSerializer()},
+        tags=['CorruptionRisk']
+    )
+    def detail_(self, request, pk=None):
+        corruption_risk = CorruptionRisk.objects.filter(id=pk).first()
+        if corruption_risk is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='CorruptionRisk not found')
+        serializer = CorruptionRiskSerializer(corruption_risk, context={'request': request})
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Create Corruption Risk',
+        operation_description='Create Corruption Risk',
+        request_body=CorruptionRiskSerializer(),
+        responses={200: CorruptionRiskSerializer()},
+        tags=['CorruptionRisk']
+    )
+    def create(self, request):
+        data = request.data
+        serializer = CorruptionRiskSerializer(data=data, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Update Corruption Risk',
+        operation_description='Update Corruption Risk',
+        request_body=CorruptionRiskSerializer(),
+        responses={200: CorruptionRiskSerializer()},
+        tags=['CorruptionRisk']
+    )
+    def update(self, request, pk=None):
+        data = request.data
+        corruption_risk = CorruptionRisk.objects.filter(id=pk).first()
+        if corruption_risk is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Corruption Risk not found')
+        serializer = CorruptionRiskSerializer(corruption_risk, data=data, partial=True, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Delete Corruption Risk',
+        operation_description='Delete Corruption Risk',
+        tags=['CorruptionRisk']
+    )
+    def delete(self, request, pk=None):
+        corruption_risk = CorruptionRisk.objects.filter(id=pk).first()
+        if corruption_risk is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Corruption Risk not found')
+        corruption_risk.delete()
+        return Response(data={'result': '', 'ok': True}, status=status.HTTP_200_OK)
+
+
+class CorruptionCaseViewSet(ViewSet):
+    @swagger_auto_schema(
+        operation_summary='Corruption Case',
+        operation_description='Corruption Case List',
+        responses={200: CorruptionCaseSerializer(many=True)},
+        tags=['CorruptionCase']
+    )
+    def list(self, request):
+        corruption_case = CorruptionCase.objects.all()
+        serializer = CorruptionRiskSerializer(corruption_case, many=True, context={'request': request})
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Corruption Case',
+        operation_description='Corruption Case Details',
+        responses={200: CorruptionCaseSerializer()},
+        tags=['CorruptionCase']
+    )
+    def detail_(self, request, pk=None):
+        corruption_case = CorruptionCase.objects.filter(id=pk).first()
+        if corruption_case is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Corruption Case not found')
+        serializer = CorruptionCaseSerializer(corruption_case, context={'request': request})
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Corruption Case',
+        operation_description='Create Corruption Case',
+        request_body=CorruptionCaseSerializer(),
+        responses={200: CorruptionCaseSerializer()},
+        tags=['CorruptionCase']
+    )
+    def create(self, request):
+        data = request.data
+        serializer = CorruptionCaseSerializer(data=data, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Corruption Case',
+        operation_description='Update Corruption Case',
+        request_body=CorruptionCaseSerializer(),
+        responses={200: CorruptionCaseSerializer()},
+        tags=['CorruptionCase']
+    )
+    def update(self, request, pk=None):
+        data = request.data
+        corruption_case = CorruptionCase.objects.filter(id=pk).first()
+        if corruption_case is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Corruption Case not found')
+        serializer = CorruptionCaseSerializer(corruption_case, data=data, partial=True, context={'request': request})
+        if not serializer.is_valid():
+            raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message=serializer.errors)
+        serializer.save()
+        return Response(data={'result': serializer.data, 'ok': True}, status=status.HTTP_200_OK)
+
+    @swagger_auto_schema(
+        operation_summary='Corruption Case',
+        operation_description='Delete Corruption Case',
+        tags=['CorruptionCase']
+    )
+    def delete(self, request, pk=None):
+        corruption_case = CorruptionCase.objects.filter(id=pk).first()
+        if corruption_case is None:
+            raise CustomApiException(ErrorCodes.NOT_FOUND, message='Corruption Case not found')
+        corruption_case.delete()
         return Response(data={'result': '', 'ok': True}, status=status.HTTP_200_OK)
